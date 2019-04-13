a09fdeab9a0bedaed8f8730df290f8b35823e759
dubesy <49588582+dubesy@users.noreply.github.com>
2019-04-13 14:49:51 +0200

    beter

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+let stappen = 0
+input.onGesture(Gesture.Shake, function () {
+    stappen += 1
+    basic.showNumber(stappen)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(stappen)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    stappen = 0
+    basic.showNumber(stappen)
+    basic.pause(200)
+    basic.clearScreen()
+})
+ 
